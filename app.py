3ea17e9b224312efbe608b1c57dd6b1aec83f433
Salaz <salaz055.ds@gmail.com>
2025-03-19 19:50:29 -0500

    Commiting changes to app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from analyze import get_sentiment, compute_embeddings, classify_email
+import json
+
 app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
@@ -48,6 +50,28 @@ def classify_with_get():
     classifications = classify_email(text)
     return jsonify({"message": "Email classified", "classifications": classifications}), 200
 
+@app.route('/api/v1/add-class/', methods =['POST'])
+def add_class():
+    r = request.json
+    
+    if "Class" not in r:
+        return jsonify({'ERROR' : "Please provide a class name ex. Class:Sports "}) , 400
+        
+    api_class = r['Class']
+    
+    
+    with open('all_classes.json', 'r') as file:
+        classes = json.load(file)
+        
+    classes.append(api_class)
+    
+    # adding the new class to the all_classes.json file
+    with open('all_classes.json', 'w') as file:
+        json.dump(classes,file)
+    
+    return jsonify({"message" : f"Class {api_class} has been added to all_classes.json", "current_classes" : classes}) , 200
+    
+        
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000, debug=True)
